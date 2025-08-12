dc46726d3ccef736043949fbd9c5639888f9f37e
Kasodariya Deep <deepkasodariya44@gmail.com>
2025-08-12 14:52:38 +0530

    Who is your message broker <3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
  * 1. Sharding (Apache Sharding Server).
  * 2. Erasure Coding.
  * 3. Pre Signed URL.
+ * 4. Kafka (Transactions)
  * */
 public class Main {
     public static void main(String[] args) {
